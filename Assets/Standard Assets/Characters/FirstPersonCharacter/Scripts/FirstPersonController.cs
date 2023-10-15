04385fac96db9c6f6c63c30848832e002e41df85
vcaraba <143634264+vcaraba@users.noreply.github.com>
2023-10-15 14:07:43 -0400

    windows

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -256,5 +256,12 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
         }
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Door"))
+            {
+                other.gameObject.SetActive(false);
+            }
+        }
     }
 }
